e82e99f540d06c557728918f04fa1cc13ea6ecc4
HarshMangaraj <harshmangaraj723@gmail.com>
2025-06-13 17:11:25 +0530

    Add api fetching gunctionality to dashboard

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -1,9 +1,38 @@
+import { Button } from "@/components/ui/button"
+import { useGeolocation } from "@/hooks/use-geolocation"
+import { RefreshCw } from "lucide-react"
 
 
-const weatherdashboard = () => {
+const WeatherDashboard = () => {
+  const {coordinates, error:locationError, getLocation, isLoading:locationLoading} = useGeolocation();
+
+  console.log(coordinates)
+
+  const handleRefresh=()=>{
+    getLocation()
+    if(coordinates){
+      //reload weather data
+    }
+  }
+
+  if(locationLoading){
+    
+  }
+
   return (
-    <div>weather-dashboard</div>
+    <div className="space-y-4">
+      {/* favorite cities */}
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl font-bold tracking-tight">My Location</h1>
+        <Button variant={'outline'} size={'icon'} onClick={handleRefresh} >
+          <RefreshCw className="h-4 w-4"/>
+        </Button>
+
+      </div>
+
+      {/* current and early weather */}
+    </div>
   )
 }
 
-export default weatherdashboard
+export default WeatherDashboard
