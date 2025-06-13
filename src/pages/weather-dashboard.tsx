e26dcbe7f6202bbee168b2e9cc9711d7c8f319be
HarshMangaraj <harshmangaraj723@gmail.com>
2025-06-13 17:34:10 +0530

    Add Coordinates error handler

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -41,6 +41,21 @@ const WeatherDashboard = () => {
     );
   }
 
+  if (!coordinates) {
+    return (
+      <Alert variant="destructive">
+        <AlertTitle>Location Required</AlertTitle>
+        <AlertDescription className="flex flex-col gap-4">
+          <p>Please enable location access to see your local  weather.</p>
+          <Button onClick={getLocation} variant={"outline"} className="w-fit">
+            <MapPin mr-2 h-4 w-4 />
+            Enable Location
+          </Button>
+        </AlertDescription>
+      </Alert>
+    );
+  }
+
   return (
     <div className="space-y-4">
       {/* favorite cities */}
